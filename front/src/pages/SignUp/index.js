b021b6f80e0d8780738b75a369e1022c3498f028
Hugo Mesquita <hugo.mesquita@itriad.org.br>
2022-10-04 21:24:16 -0400

    chore: modify function  signUp

--- front/src/pages/SignUp/index.js
+++ front/src/pages/SignUp/index.js
@@ -1,21 +1,58 @@
-import { useState, useContext } from 'react';
-
-import { AuthContext } from '../../contexts/auth'
+import { useState } from 'react';
+import api from '../../services/api';
 import { Link } from 'react-router-dom';
+import { toast } from "react-toastify";
 import './signup.css';
 
 function SignUp() {
-    const [login, setLogin] = useState('');
+    const [name, setName] = useState('');
+    const [lastname, setLastName] = useState('');
+    const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [loadingAuth, setLoadingAuth] = useState(false);
+ 
 
-    const { signIn, loadingAuth } = useContext(AuthContext);
 
-
-    const handleSubmit = (e) => {
+    async function handleSubmit(e) {
       e.preventDefault();
       
-      if(login !== '' && password !== ''){
-        signIn(login, password);
+
+      if(name !== '' && lastname !== '' && email !== '' && password !== ''){
+    
+        await api.post('/users',{
+          name,
+          lastname,
+          email,
+          password,
+          access_level: 2
+        })
+        .then(async (response) => {
+            
+            if(response.data !== false){
+
+                setLoadingAuth(false);
+                toast.success('Cadastro efetuado com Sucesso!');
+                setName('');
+                setLastName('');
+                setEmail('');
+                setPassword('');
+
+          
+            }else{
+                toast.error('Ops, algo deu errado!');
+                setLoadingAuth(false);
+            }
+
+
+        })
+        .catch((error)=>{
+            console.log(error);
+            toast.error('Ops, algo deu errado!');
+            setLoadingAuth(false);
+        })
+
+
+
       }
 
     }
@@ -33,7 +70,7 @@ function SignUp() {
 
                 <div className="wrapper">
                     <form onSubmit={handleSubmit}>
-                        <div class="flex space">
+                        <div className="flex space">
                             <h2>Cadastre-se</h2>
                             <Link to="">Voltar</Link>
                         </div>
@@ -41,15 +78,25 @@ function SignUp() {
 
                         <div className="form-group">
                             <div className="col-2">
-                                <input type="text" placeholder="Nome" />
+                                <input type="text" placeholder="Nome" 
+                                          value={name} 
+                                          onChange={(e) => setName(e.target.value)}/>
                             </div>
                             <div className="col-2">
-                                <input type="text" placeholder="Sobrenome" />
+                                <input type="text" placeholder="Sobrenome" 
+                                          value={lastname}
+                                          onChange={(e) => setLastName(e.target.value)}/>
                             </div>
                         </div>
 
-                        <input type="text" placeholder="E-Mail" value={login} onChange={(e) => setLogin(e.target.value)}/>
-                        <input type="password" placeholder="Senha" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                        <input type="text" placeholder="E-Mail" 
+                                          value={email} 
+                                          onChange={(e) => setEmail(e.target.value)}/>
+                        <input type="password" placeholder="Senha" 
+                                          value={password} 
+                                          onChange={(e) => setPassword(e.target.value)}/>
+
+
                         <button type="submit">{loadingAuth ? 'Carregando...' : 'Cadastrar'}</button>
                         
                     </form>
